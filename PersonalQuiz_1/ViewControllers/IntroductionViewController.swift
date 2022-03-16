fa8f37b97d3a0f617b0767b8a2d53126858a5b7e
venzhi87 <anarhy87@list.ru>
2022-03-16 21:24:33 +0300

    done story boards and threaded code (without result)

---
+++ PersonalQuiz_1/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz_1
+//
+//  Created by Pavel Kuzovlev on 13.03.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    @IBAction func unwind(for segue: UIStoryboardSegue) {}
+
+}
+
---
+++ PersonalQuiz_1/ViewControllers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz_1
+//
+//  Created by Pavel Kuzovlev on 15.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // 1. Передать массив с ответами на экран с результатами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты в соответствии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
